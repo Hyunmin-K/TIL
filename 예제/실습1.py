6d0aece966395d6126408a5f259691fef4104a5e
Hyunmin-K <tkfehowl@naver.com>
2023-01-02 16:17:23 +0900

    example_0102

---
+++ 예제/실습1.py
@@ -0,0 +1,68 @@
+# number = int(input("정수형 숫자를 입력해주세요 > "))
+# print(number+10)
+
+# food = input("좋아하는 음식을 입력해주세요 > ")
+# print(food)
+
+# name = input("이름을 입력해주세요 > ")
+# year = int(input("태어난 년도를 입력해주세요 > "))
+# age = 2023-year+1
+# print("저의 이름은 %s이고, 올해 나이는 %d세 입니다."%(name, age))
+
+# first_line = input("첫 번째 문장을 입력해주세요 > ")
+# second_line = input("두 번째 문장을 입력해주세요 > ")
+# line = first_line + second_line
+# print(line)
+
+# number = int(input("숫자를 입력해주세요 > "))
+# reverse = number * (-1)
+# print(reverse)
+
+# number1 = int(input("첫 번째 숫자를 입력해주세요 > "))
+# number2 = int(input("두 번째 숫자를 입력해주세요 > "))
+# plus = number1 + number2
+# minus = number1 - number2
+# multiply = number1 * number2
+# share = number1 // number2
+# rest = number1 % number2
+# print("더하기 연산 : %d" %plus)
+# print("빼기 연산 : %d" %minus)
+# print("곱하기 연산 : %d" %multiply)
+# print("몫 연산 : %d" %share)
+# print("나머지 연산 : %d" %rest)
+
+# number1 = int(input("첫 번째 정수형 숫자를 입력해주세요 > "))
+# number2 = int(input("두 번째 정수형 숫자를 입력해주세요 > "))
+# number3 = int(input("세 번째 정수형 숫자를 입력해주세요 > "))
+# average = int((number1 + number2 + number3) / 3)
+# print(average)
+
+# number1 = int(input("첫 번째 정수형 숫자를 입력해주세요 > "))
+# number2 = int(input("두 번째 정수형 숫자를 입력해주세요 > "))
+# number3 = int(input("세 번째 정수형 숫자를 입력해주세요 > "))
+# number4 = int(input("네 번째 정수형 숫자를 입력해주세요 > "))
+# number5 = int(input("다섯 번째 정수형 숫자를 입력해주세요 > "))
+# num_list = [number1, number2, number3, number4, number5]
+# print(num_list)
+
+# string = input("문자열을 입력해주세요 > ")
+# number = int(input("정수형 숫자를 입력해주세요 > "))
+# multiple = string * number
+# print(multiple)
+
+sum = 0
+number1 = int(input("첫 번째 정수형 숫자를 입력해주세요 > "))
+sum += number1
+print(sum)
+number2 = int(input("두 번째 정수형 숫자를 입력해주세요 > "))
+sum += number2
+print(sum)
+number3 = int(input("세 번째 정수형 숫자를 입력해주세요 > "))
+sum += number3
+print(sum)
+number4 = int(input("네 번째 정수형 숫자를 입력해주세요 > "))
+sum += number4
+print(sum)
+number5 = int(input("다섯 번째 정수형 숫자를 입력해주세요 > "))
+sum += number5
+print(sum)
---
+++ 예제/예제1.py
@@ -0,0 +1,49 @@
+# number1 = 1
+# number2 = number1 + 1
+# print(number1, number2)
+
+# number1 = 10
+# number2 = 5
+# number3 = str(number1) + str(number2)
+# number4 = number1 + number2
+# print(number1, number2, number3, number4)
+
+# string1 = "Hello"
+# string2 = string1
+# string3 = "World" + "!"
+# print(string2, "?", string3)
+
+# string = "Hello?"
+# n = 5
+# print(string * n)
+
+# string = "abc hello def"
+# sub_string1 = string[4:10]
+# sub_string2 = string[1:4]
+# sub_string3 = string[-1]
+# print(sub_string1)
+# print(sub_string2)
+# print(sub_string3)
+
+# number1 = 5
+# number2 = 10.0 + number1
+# number1 -5
+# number3 = number1 * (number2 + 10)
+# print(number1, number2, number3)
+
+# list_variable = [1,2,3,[1,2,3]]
+# sub_list = list_variable[3][-1]
+# print(sub_list)
+
+# list_variable = []
+# list_variable.append(1)
+# list_variable.append("a")
+# list_variable[0] = 0
+# print(list_variable)
+
+# name = input("너의 이름은?")
+# print(name)
+
+age = int(input("너의 나이는?"))
+print("올해 나이 : ", age)
+print("내년 나이 : ", age+1)
